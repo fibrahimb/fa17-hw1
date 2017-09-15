954474b6729165c928556a69ca9e3f4f1fd87a66
Fatima Ibrahim-Biangoro <fibrahimb@berkeley.edu>
2017-09-15 12:37:24 -0700

    Completed all the questions for hw1

--- foobar.rb
+++ foobar.rb
@@ -1,8 +1,13 @@
 class Foobar
 
   def self.baz(a)
-    # Class method
-    # Call with `Foobar.baz`
+  	new_array = a.map! {|i| i.to_i}
+  	array_plus_two = new_array.map!{|i|i+2}
+  	only_even = array_plus_two.select(&:even?)
+  	remove_duplicates = only_even.uniq
+  	less_than_ten = remove_duplicates.reject! {|i| i > 10}
+  	sum_of_all = less_than_ten.inject(0) {|sum, i| sum+i}
+  	return sum_of_all
   end
 end
 
